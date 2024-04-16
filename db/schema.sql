a877d09088c20dbeb2b7a88aa3cbb28a95c07d42
Cody T <crthorn11@gmail.com>
2024-04-16 18:58:33 -0400

    Completed my schema and seeds.sql and the bugs that were in it.

--- db/schema.sql
+++ db/schema.sql
@@ -2,8 +2,6 @@ DROP DATABASE IF EXISTS employee_tracker;
 
 CREATE DATABASE employee_tracker;
 
-SHOW DATABASES;
-
 USE employee_tracker;
 
 CREATE TABLE employees (
@@ -13,22 +11,16 @@ CREATE TABLE employees (
     gender       ENUM ('M', 'F') NOT NULL,
     hire_date    DATE            NOT NULL,
     PRIMARY KEY  (emp_no)
-);.
+);
 
 CREATE TABLE departments (
-    dept_no     CHAR(4)        NOT NULL,
-    dept_name   VARCHAR(40)    NOT NULL,
+    dept_no     INT            NOT NULL,
+    dept_name   VARCHAR(16)    NOT NULL,
     PRIMARY KEY (dept_no, dept_name)
 );
 
 CREATE TABLE department_manager (
     emp_no     INT                  NOT NULL,
-    dept_no    CHAR(4)              NOT NULL,
-    from_date  DATE                 NOT NULL,
-    to_date    DATE                 NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no) ON DELETE CASCADE,
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no) ON DELETE CASCADE,
-PRIMARY KEY (emp_no, dept_no)
+    dept_no    INT                  NOT NULL,
+    PRIMARY KEY (emp_no, dept_no)
 );
-
--- why isnt my tables showing? :(
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,14 @@
 INSERT INTO employees (emp_no, first_name, last_name, gender, hire_date)
-VALUES (001, 'Cody', 'Yonker', 'M', '06/31/2004'),
-       (002, 'Frank', 'Kronk' 'M', '12/17/2007'),
-       (003, 'Sarah', 'Ramp', 'F', '04/15/2010'),
-       (004, 'Tom', 'Willt' 'M', '09/03/2018'),
-       (005, 'Alexis', 'Ammde', 'F', '01/31/2020');
+VALUES (001, 'Cody', 'Yonker', 'M', '2004-02-04'),
+       (002, 'Frank', 'Kronk', 'M', '2007-12-17'),
+       (003, 'Sarah', 'Ramp', 'F', '2013-04-15'),
+       (004, 'Tom', 'Willt', 'M', '2018-09-04'),
+       (005, 'Alexis', 'Ammde', 'F', '2020-01-31');
 
 INSERT INTO departments (dept_no, dept_name)
-VALUES (010, 'Parts'),
-       (020, 'Manufacturing');
+VALUES (006, 'Parts'),
+       (007, 'Manufacturing');
 
-INSERT INTO department_manager (emp_no, dept_no, from_date, to_date)
-VALUES (0100, 'Parts'),
-       (0200, 'Manufacturing');
+INSERT INTO department_manager (emp_no, dept_no)
+VALUES (002, 006),
+       (001, 007);
