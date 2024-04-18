de34801a77adbbd67d8b75d6817166c6c00f0267
Cody T <crthorn11@gmail.com>
2024-04-17 22:51:27 -0400

    k

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// Import and require mysql2
+const inquirer = require('inquirer');
 const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
@@ -10,28 +10,95 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Connect to database
-const db = mysql.createConnection(
-  {
-    host: 'localhost',
-    // MySQL username,
-    user: 'root',
-    // MySQL password
-    password: '',
-    database: 'employee_tracker'
-  },
-  console.log(`Connected to the employee_tracker database.`)
-);
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'employee_tracker'
+}, console.log(`Connected to the employee_tracker database.`));
 
 // Query database
-db.query('SELECT * FROM employee_tracker', function (err, results) {
-  console.log(results);
-});
+// db.query('SELECT * FROM employees', function (err, results) {
+//   console.log(results);
+// });
+const viewAllEmployees = () => {
+  const query = 'SELECT * FROM employees';
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error('Error retrieving employees:', err);
+      return;
+    }
+    console.log('All Employees:');
+    console.table(results); // Using console.table to nicely display the results in a table format
+  });
+};
 
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-  res.status(404).end();
-});
+//Inquirer prompts below
 
+const promptUser = () => {
+  inquirer.prompt([
+    {
+      name: 'choices',
+      type: 'list',
+      message: 'Please select an option:',
+      choices: [
+        'View All Employees',
+        'View All Roles',
+        'View All Departments',
+        'View All Employees By Department',
+        'View Department Budgets',
+        'Update Employee Role',
+        'Update Employee Manager',
+        'Add Employee',
+        'Add Role',
+        'Add Department',
+        'Exit'
+      ]
+    }
+  ]).then((answers) => {
+    const { choices } = answers;
+
+    if (choices === 'View All Employees') {
+      viewAllEmployees();
+    }
+
+    if (choices === 'View All Departments') {
+      viewAllDepartments();
+    }
+
+    if (choices === 'View All Employees By Department') {
+      viewEmployeesByDepartment();
+    }
+
+    if (choices === 'Add Employee') {
+      addEmployee();
+    }
+
+    if (choices === 'Update Employee Role') {
+      updateEmployeeRole();
+    }
+
+    if (choices === 'View All Roles') {
+      viewAllRoles();
+    }
+
+    if (choices === 'Add Role') {
+      addRole();
+    }
+
+    if (choices === 'Add Department') {
+      addDepartment();
+    }
+
+    if (choices === 'Exit') {
+      connection.end();
+    }
+  });
+};
+
+promptUser();
+
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
