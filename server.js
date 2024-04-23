d7d6a7217899fbc9352221026d5692e8d52c1d86
Cody T <crthorn11@gmail.com>
2024-04-22 22:08:09 -0400

    Added the functions to view all and add to the department, role and employee tables

--- server.js
+++ server.js
@@ -12,6 +12,18 @@ const db = mysql.createConnection({
 }, console.log(`Connected to the employee_tracker database.`));
 
 // Query database
+const viewAllRole = () => {
+  const query = 'SELECT * FROM role';
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error('Error retrieving employee:', err);
+      return;
+    }
+    console.table(results);
+    promptUser();
+  });
+};
+
 const viewAllEmployee = () => {
   const query = 'SELECT * FROM employee';
   db.query(query, (err, results) => {
@@ -24,9 +36,132 @@ const viewAllEmployee = () => {
   });
 };
 
-//  const viewAllRoles = () = => {
-//    const query = ''
-//  }
+const viewAllDepartment = () => {
+  const query = 'SELECT * FROM department';
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error('Error retrieving employee:', err);
+      return;
+    }
+    console.table(results);
+    promptUser();
+  });
+};
+
+const addDepartment = () => {
+  return new Promise((resolve, reject) => {
+    inquirer.prompt([
+      {
+        name: 'dept_id',
+        type: 'input',
+        message: "Enter the Department ID:"
+      },
+      {
+        name: 'dept_name',
+        type: 'input',
+        message: "Enter the department name:"
+      },
+    ]).then((answers) => {
+      const { dept_id, dept_name } = answers;
+
+      const query = 'INSERT INTO department (dept_id, dept_name) VALUES (?, ?)';
+      db.query(query, [dept_id, dept_name], (err, result) => {
+        if (err) {
+          console.error('Error adding department:', err);
+          reject(err);
+        } else {
+          console.log('Department added successfully!');
+          resolve();
+        }
+      });
+    });
+  });
+};
+
+const addRole = () => {
+  return new Promise((resolve, reject) => {
+    inquirer.prompt([
+      {
+        name: 'job_title',
+        type: 'input',
+        message: "Enter the Role Job Title:"
+      },
+      {
+        name: 'role_id',
+        type: 'input',
+        message: "Enter the role ID:"
+      },
+      {
+        name: 'dept_id',
+        type: 'input',
+        message: "Enter the department id:"
+      },
+      {
+        name: 'salary',
+        type: 'input',
+        message: "Enter the role salary:"
+      },
+    ]).then((answers) => {
+      const { job_title, role_id, dept_id, salary } = answers;
+
+      const query = 'INSERT INTO role (job_title, role_id, dept_id, salary) VALUES (?, ?, ?, ?)';
+      db.query(query, [ job_title, role_id, dept_id, salary ], (err, result) => {
+        if (err) {
+          console.error('Error adding role:', err);
+          reject(err);
+        } else {
+          console.log('Role added successfully!');
+          resolve();
+        }
+      });
+    });
+  });
+};
+
+const addEmployee = () => {
+  return new Promise((resolve, reject) => {
+    inquirer.prompt([
+      {
+        name: 'emp_id',
+        type: 'input',
+        message: "Enter the employee's ID:"
+      },
+      {
+        name: 'first_name',
+        type: 'input',
+        message: "Enter the employee's first name:"
+      },
+      {
+        name: 'last_name',
+        type: 'input',
+        message: "Enter the employee's last name:"
+      },
+      {
+        name: 'role_id',
+        type: 'input',
+        message: "Enter the employee's role ID:"
+      },
+      {
+        name: 'manager_id',
+        type: 'input',
+        message: "Enter the employee's manager ID (if applicable):"
+      }
+    ]).then((answers) => {
+      const { emp_id, first_name, last_name, role_id, manager_id } = answers;
+
+      const query = 'INSERT INTO employee (emp_id, first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?, ?)';
+      db.query(query, [ emp_id, first_name, last_name, role_id, manager_id], (err, result) => {
+        if (err) {
+          console.error('Error adding employee:', err);
+          reject(err);
+        } else {
+          console.log('Employee added successfully!');
+          resolve();
+        }
+      });
+    });
+  });
+};
 
 //Inquirer prompts below
 
@@ -37,27 +172,24 @@ const promptUser = () => {
       type: 'list',
       message: 'Please select an option:',
       choices: [
-        'View All Employees',
-        'View All Roles',
-        'View All Departments',
-        'View All Employees By Department',
-        'View Department Budgets',
-        'Update Employee Role',
-        'Update Employee Manager',
+        'View All Employee',
+        'View All Role',
+        'View All Department',
         'Add Employee',
         'Add Role',
         'Add Department',
+        'Exit'
       ]
     }
   ]).then((answers) => {
     const { choices } = answers;
 
-    if (choices === 'View All Employees') {
-      viewAllEmployees();
+    if (choices === 'View All Employee') {
+      viewAllEmployee();
     }
 
-    if (choices === 'View All Departments') {
-      viewAllDepartments();
+    if (choices === 'View All Department') {
+      viewAllDepartment();
     }
 
     if (choices === 'View All Employees By Department') {
@@ -72,8 +204,8 @@ const promptUser = () => {
       updateEmployeeRole();
     }
 
-    if (choices === 'View All Roles') {
-      viewAllRoles();
+    if (choices === 'View All Role') {
+      viewAllRole();
     }
 
     if (choices === 'Add Role') {
