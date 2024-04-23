bebe6a4e7407a13f0cbc8b8624cf6277a0674bfd
Cody T <crthorn11@gmail.com>
2024-04-22 20:54:06 -0400

    Fixed bug related to the value in my role seed

--- db/schema.sql
+++ db/schema.sql
@@ -4,6 +4,22 @@ CREATE DATABASE employee_tracker;
 
 USE employee_tracker;
 
+CREATE TABLE department (
+    dept_id     INT            NOT NULL,
+    dept_name   VARCHAR(30)    NOT NULL,
+    PRIMARY KEY (dept_id)
+);
+
+CREATE TABLE role (
+    job_title    VARCHAR(30)     NOT NULL,
+    role_id      INT             NOT NULL,
+    dept_id      INT             NOT NULL,
+    salary       DECIMAL         NOT NULL,
+    PRIMARY KEY  (role_id),
+    FOREIGN KEY  (dept_id) REFERENCES department(dept_id)
+);
+
+
 CREATE TABLE employee (
     emp_id       INT             NOT NULL,
     first_name   VARCHAR(14)     NOT NULL,
@@ -11,22 +27,7 @@ CREATE TABLE employee (
     role_id      INT             NOT NULL,
     manager_id   INT,            
     PRIMARY KEY  (emp_id),
-    FOREIGN KEY (role_id) references role(role_id),
-    FOREIGN KEY (manager_id) references employee(emp_id)
-);
-
-CREATE TABLE role (
-    job_title    VARCHAR(30)     NOT NULL,
-    role_id      INT             NOT NULL,
-    dept_id      INT             NOT NULL,
-    salary       INT             NOT NULL,
-    PRIMARY KEY  (role_id),
-    FOREIGN KEY   (dept_id) references department(dept_id)
-);
-
-CREATE TABLE department (
-    dept_id     INT            NOT NULL,
-    dept_name   VARCHAR(16)    NOT NULL,
-    PRIMARY KEY (dept_id)
+    FOREIGN KEY (role_id) REFERENCES role(role_id),
+    FOREIGN KEY (manager_id) REFERENCES employee(emp_id)
 );
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,15 +1,16 @@
-INSERT INTO employee (emp_id, first_name, last_name, role_id, manager_id)
-VALUES (001, 'Cody', 'Yonker', 30, 001),
-       (002, 'Frank', 'Kronk', 20, 200),
-       (003, 'Sarah', 'Ramp', 10, 200),
-       (004, 'Tom', 'Willt', 10, 200),
-       (005, 'Alexis', 'Ammde',10, 300);
-
-INSERT INTO departments (dept_id, dept_name)
-VALUES (200, 'Parts'),
+INSERT INTO department (dept_id, dept_name)
+VALUES (100, 'Management'),
+       (200, 'Parts'),
        (300, 'Manufacturing');
 
 INSERT INTO role (job_title, role_id, dept_id, salary)
-VALUES ('Manufacturing_manager', 30, 300, 156700),
-       ('Parts_manager', 20, 200, 115300),
-       ('Worker', 10, 100, 60000);
+VALUES ('Manufacturing_manager', 30, 300, 2000.00),
+       ('Parts_manager', 20, 200, 1500.00),
+       ('Manager', 10, 100, 1800.00);
+
+INSERT INTO employee (emp_id, first_name, last_name, role_id, manager_id)
+VALUES (001, 'Cody', 'Yonker', 30, 1),
+       (002, 'Frank', 'Kronk', 20, 2),
+       (003, 'Sarah', 'Ramp', 10, 2),
+       (004, 'Tom', 'Willt', 10, 2),
+       (005, 'Alexis', 'Ammde',10, 3);
